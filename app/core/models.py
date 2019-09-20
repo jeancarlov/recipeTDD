9777aff0e081985fcf2d2d28cf0a671c3f92a9d5
Jean Valderrama <jeanvalderrama@Jeans-MacBook-Pro.local>
2019-09-20 18:31:42 -0400

    changes made to comply with travis-ci

--- app/core/models.py
+++ app/core/models.py
@@ -1,19 +1,21 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
-
-#manager clas
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
+                                        PermissionsMixin
+# manager class
 class userManager(BaseUserManager):
     def create_user(self,email,password=None,**extra_fields):
         # creates and saves a new user
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
+        user = self.model(email=email, **extra_fields) 
+        user.set_password(password) 
+        user.save(using=self._db) 
 
         return user
 
 # User model class that supports using email instead of username
 class User (AbstractBaseUser, PermissionsMixin):
-    email =models.EmailField(max_length=255, unique=True)
+
+
+    email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
